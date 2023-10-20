a3932fe9478e9c8364748cac3ba0e8ef956b7dce
Mateusz Nowak <mateusz.p.nowak@intel.com>
2023-10-20 18:48:05 +0200

    rebuild for not fixed nodes number

--- test.cpp
+++ test.cpp
@@ -7,28 +7,19 @@
 #include <iterator>
 #include <vector>
 
-#define USE_SYCL
+#define USM_SHARED_MEM
 
-int comm_size_, comm_rank_;
+#define SIZE 1000
 
+#ifdef USM_SHARED_MEM
 template <typename T>
-std::vector<T> generate_random(std::size_t n, std::size_t bound = 25) {
-  std::vector<T> v;
-  v.reserve(n);
-
-  for (std::size_t i = 0; i < n; i++) {
-    auto r = lrand48() % bound;
-    v.push_back(r);
-  }
-  return v;
-}
-
-#ifdef USE_SYCL
-template <typename T>
-using V = std::vector<T, sycl::usm_allocator<T, sycl::usm::alloc::shared>>;
+using Alloc = sycl::usm_allocator<T, sycl::usm::alloc::shared>;
 #else
-template <typename T> using V = std::vector<T>;
+template <typename T>
+using Alloc = sycl::usm_allocator<T, sycl::usm::alloc::host>;
 #endif
+
+template <typename T> using V = std::vector<T, Alloc<T>>;
 
 template <typename T>
 void alltoallv(const V<T> &sendbuf, const std::vector<int> &sendcnt,
@@ -48,6 +39,7 @@ void alltoallv(const V<T> &sendbuf, const std::vector<int> &sendcnt,
                 std::data(recvdsp), MPI_BYTE, MPI_COMM_WORLD);
 }
 
+int comm_size_, comm_rank_;
 int main(int argc, char **argv) {
   using T = int;
   std::cout << "main()" << std::endl;
@@ -56,13 +48,7 @@ int main(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &comm_size_);
   MPI_Comm_rank(MPI_COMM_WORLD, &comm_rank_);
 
-  sycl::queue q(sycl::gpu_selector_v);
-  std::cout << comm_rank_ << ": Running on: "
-            << q.get_device().get_info<sycl::info::device::name>() << std::endl;
-
-  // int size = 135167;
-  const int fullsize = 8000000;
-  int size = fullsize / comm_size_;
+  int size = SIZE;
 
   std::cout << comm_rank_ << ": MPI Initialized, size " << comm_size_
             << std::endl;
@@ -72,33 +58,24 @@ int main(int argc, char **argv) {
       recvcnt(comm_size_), recvdsp(comm_size_);
 
   std::size_t recvsize = 0;
-
-  for (int i = 0; i < comm_size_; i++) {
-    sendcnt[i] = recvcnt[i] = size;
-    senddsp[i] = recvdsp[i] = i * size;
-    recvsize += recvcnt[i];
+  for (int _i = 0; _i < comm_size_; _i++) {
+    sendcnt[_i] = size;
+    senddsp[_i] = _i * size;
+    recvcnt[_i] = size;
+    recvdsp[_i] = _i * size;
+    recvsize += recvcnt[_i];
   }
 
-  // sendcnt = {size, size, size, size};
-  // senddsp = {0, size, 2 * size, 3 * size};
-  // recvcnt = {size, size, size, size};
-  // recvdsp = {0, size, 2 * size, 3 * size};
+  sycl::queue q;
+  Alloc<T> alloc(q);
 
-  // std::size_t recvsize = recvcnt[0] + recvcnt[1] + recvcnt[2] + recvcnt[3];
-
-#ifdef USE_SYCL
-  std::cout << "With SYCL alloc" << std::endl;
-  sycl::usm_allocator<T, sycl::usm::alloc::shared> alloc(q);
   std::vector<T, decltype(alloc)> vs(size, alloc);
   std::vector<T, decltype(alloc)> vr(recvsize, alloc);
-#else
-  std::cout << "With default alloc" << std::endl;
-  std::vector<T> vs = generate_random<T>(size, 1000);
-  std::vector<T> vr(recvsize);
-#endif
 
   std::cout << "alltoallv()" << std::endl;
+
   alltoallv(vs, sendcnt, senddsp, vr, recvcnt, recvdsp);
+
   std::cout << "end" << std::endl;
   MPI_Finalize();
 }
